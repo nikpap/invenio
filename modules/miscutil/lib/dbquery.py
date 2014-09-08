0401484cec463cdb5086c5fbbc93ef7406afbccd
Alessio Deiana <adeiana@gmail.com>
2014-09-08 10:46:46 +0200

    dbquery: cleanup

--- modules/miscutil/lib/dbquery.py
+++ modules/miscutil/lib/dbquery.py
@@ -29,18 +29,15 @@ __revision__ = "$Id$"
 
 # dbquery clients can import these from here:
 # pylint: disable=W0611
-from MySQLdb import Warning, Error, InterfaceError, DataError, \
-                    DatabaseError, OperationalError, IntegrityError, \
-                    InternalError, NotSupportedError, \
-                    ProgrammingError
+from MySQLdb import (Warning, Error, InterfaceError, DataError,
+                     DatabaseError, OperationalError, IntegrityError,
+                     InternalError, NotSupportedError, ProgrammingError)
 import gc
 import os
-import string
 import time
 import marshal
 import re
 import atexit
-import os
 
 from zlib import compress, decompress
 from thread import get_ident
@@ -49,45 +46,31 @@ from invenio.config import CFG_ACCESS_CONTROL_LEVEL_SITE, \
     CFG_MISCUTIL_SQL_RUN_SQL_MANY_LIMIT
 
 if CFG_MISCUTIL_SQL_USE_SQLALCHEMY:
-    try:
-        import sqlalchemy.pool as pool
-        import MySQLdb as mysqldb
-        mysqldb = pool.manage(mysqldb, use_threadlocal=True)
-        connect = mysqldb.connect
-    except ImportError:
-        CFG_MISCUTIL_SQL_USE_SQLALCHEMY = False
-        from MySQLdb import connect
+    import sqlalchemy.pool as pool
+    import MySQLdb as mysqldb
+    mysqldb = pool.manage(mysqldb, use_threadlocal=True)
+    connect = mysqldb.connect
 else:
     from MySQLdb import connect
 
-## DB config variables.  These variables are to be set in
-## invenio-local.conf by admins and then replaced in situ in this file
-## by calling "inveniocfg --update-dbexec".
-## Note that they are defined here and not in config.py in order to
-## prevent them from being exported accidentally elsewhere, as no-one
-## should know DB credentials but this file.
-## FIXME: this is more of a blast-from-the-past that should be fixed
-## both here and in inveniocfg when the time permits.
-try:
-    from invenio.dbquery_config import CFG_DATABASE_HOST, \
-                                       CFG_DATABASE_PORT, \
-                                       CFG_DATABASE_NAME, \
-                                       CFG_DATABASE_USER, \
-                                       CFG_DATABASE_PASS, \
-                                       CFG_DATABASE_SLAVE, \
-                                       CFG_DATABASE_SLAVE_SU_USER, \
-                                       CFG_DATABASE_SLAVE_SU_PASS, \
-                                       CFG_DATABASE_PASSWORD_FILE
-except ImportError:
-    CFG_DATABASE_HOST = 'localhost'
-    CFG_DATABASE_PORT = '3306'
-    CFG_DATABASE_NAME = 'invenio'
-    CFG_DATABASE_USER = 'invenio'
-    CFG_DATABASE_PASS = 'my123p$ss'
-    CFG_DATABASE_SLAVE = ''
-    CFG_DATABASE_SLAVE_SU_USER = ''
-    CFG_DATABASE_SLAVE_SU_PASS = ''
-    CFG_DATABASE_PASSWORD_FILE = ''
+# DB config variables.  These variables are to be set in
+# invenio-local.conf by admins and then replaced in situ in this file
+# by calling "inveniocfg --update-dbexec".
+# Note that they are defined here and not in config.py in order to
+# prevent them from being exported accidentally elsewhere, as no-one
+# should know DB credentials but this file.
+# FIXME: this is more of a blast-from-the-past that should be fixed
+# both here and in inveniocfg when the time permits.
+from invenio.dbquery_config import (CFG_DATABASE_HOST,
+                                    CFG_DATABASE_PORT,
+                                    CFG_DATABASE_NAME,
+                                    CFG_DATABASE_USER,
+                                    CFG_DATABASE_PASS,
+                                    CFG_DATABASE_SLAVE,
+                                    CFG_DATABASE_SLAVE_SU_USER,
+                                    CFG_DATABASE_SLAVE_SU_PASS,
+                                    CFG_DATABASE_PASSWORD_FILE)
+
 
 def _get_password_from_database_password_file(user):
     """
@@ -110,17 +93,18 @@ _DB_CONN = {}
 _DB_CONN[CFG_DATABASE_HOST] = {}
 _DB_CONN[CFG_DATABASE_SLAVE] = {}
 
+
 def get_connection_for_dump_on_slave():
     """
     Return a valid connection, suitable to perform dump operation
     on a slave node of choice.
     """
     connection = connect(host=CFG_DATABASE_SLAVE,
-                                         port=int(CFG_DATABASE_PORT),
-                                         db=CFG_DATABASE_NAME,
-                                         user=CFG_DATABASE_SLAVE_SU_USER,
-                                         passwd=CFG_DATABASE_SLAVE_SU_PASS,
-                                         use_unicode=False, charset='utf8')
+                         port=int(CFG_DATABASE_PORT),
+                         db=CFG_DATABASE_NAME,
+                         user=CFG_DATABASE_SLAVE_SU_USER,
+                         passwd=CFG_DATABASE_SLAVE_SU_PASS,
+                         use_unicode=False, charset='utf8')
     connection.autocommit(True)
     return connection
 
@@ -128,33 +112,35 @@ def get_connection_for_dump_on_slave():
 def unlock_all():
     for dbhost in _DB_CONN.keys():
         for db in _DB_CONN[dbhost].values():
-            try:
-                cur = db.cur()
-                cur.execute("UNLOCK TABLES")
-            except:
-                pass
+            cur = db.cur()
+            cur.execute("UNLOCK TABLES")
 
 atexit.register(unlock_all)
 
+
 class InvenioDbQueryWildcardLimitError(Exception):
+
     """Exception raised when query limit reached."""
+
     def __init__(self, res):
         """Initialization."""
+        super(InvenioDbQueryWildcardLimitError, self).__init__()
         self.res = res
+
 
 def _db_login(dbhost=CFG_DATABASE_HOST, relogin=0):
     """Login to the database."""
 
-    ## Note: we are using "use_unicode=False", because we want to
-    ## receive strings from MySQL as Python UTF-8 binary string
-    ## objects, not as Python Unicode string objects, as of yet.
+    # Note: we are using "use_unicode=False", because we want to
+    # receive strings from MySQL as Python UTF-8 binary string
+    # objects, not as Python Unicode string objects, as of yet.
 
-    ## Note: "charset='utf8'" is needed for recent MySQLdb versions
-    ## (such as 1.2.1_p2 and above).  For older MySQLdb versions such
-    ## as 1.2.0, an explicit "init_command='SET NAMES utf8'" parameter
-    ## would constitute an equivalent.  But we are not bothering with
-    ## older MySQLdb versions here, since we are recommending to
-    ## upgrade to more recent versions anyway.
+    # Note: "charset='utf8'" is needed for recent MySQLdb versions
+    # (such as 1.2.1_p2 and above).  For older MySQLdb versions such
+    # as 1.2.0, an explicit "init_command='SET NAMES utf8'" parameter
+    # would constitute an equivalent.  But we are not bothering with
+    # older MySQLdb versions here, since we are recommending to
+    # upgrade to more recent versions anyway.
 
     if CFG_MISCUTIL_SQL_USE_SQLALCHEMY:
         return connect(host=dbhost, port=int(CFG_DATABASE_PORT),
@@ -165,25 +151,26 @@ def _db_login(dbhost=CFG_DATABASE_HOST, relogin=0):
         thread_ident = (os.getpid(), get_ident())
     if relogin:
         connection = _DB_CONN[dbhost][thread_ident] = connect(host=dbhost,
-                                         port=int(CFG_DATABASE_PORT),
-                                         db=CFG_DATABASE_NAME,
-                                         user=CFG_DATABASE_USER,
-                                         passwd=CFG_DATABASE_PASS,
-                                         use_unicode=False, charset='utf8')
+                                            port=int(CFG_DATABASE_PORT),
+                                            db=CFG_DATABASE_NAME,
+                                            user=CFG_DATABASE_USER,
+                                            passwd=CFG_DATABASE_PASS,
+                                            use_unicode=False, charset='utf8')
         connection.autocommit(True)
         return connection
     else:
-        if _DB_CONN[dbhost].has_key(thread_ident):
+        if thread_ident in _DB_CONN[dbhost]:
             return _DB_CONN[dbhost][thread_ident]
         else:
             connection = _DB_CONN[dbhost][thread_ident] = connect(host=dbhost,
-                                             port=int(CFG_DATABASE_PORT),
-                                             db=CFG_DATABASE_NAME,
-                                             user=CFG_DATABASE_USER,
-                                             passwd=CFG_DATABASE_PASS,
-                                             use_unicode=False, charset='utf8')
+                                            port=int(CFG_DATABASE_PORT),
+                                            db=CFG_DATABASE_NAME,
+                                            user=CFG_DATABASE_USER,
+                                            passwd=CFG_DATABASE_PASS,
+                                            use_unicode=False, charset='utf8')
             connection.autocommit(True)
             return connection
+
 
 def _db_logout(dbhost=CFG_DATABASE_HOST):
     """Close a connection."""
@@ -192,6 +179,7 @@ def _db_logout(dbhost=CFG_DATABASE_HOST):
     except KeyError:
         pass
 
+
 def close_connection(dbhost=CFG_DATABASE_HOST):
     """
     Enforce the closing of a connection
@@ -199,12 +187,14 @@ def close_connection(dbhost=CFG_DATABASE_HOST):
     """
     try:
         db = _DB_CONN[dbhost][(os.getpid(), get_ident())]
+    except KeyError:
+        pass
+    else:
         cur = db.cursor()
         cur.execute("UNLOCK TABLES")
         db.close()
         del _DB_CONN[dbhost][(os.getpid(), get_ident())]
-    except KeyError:
-        pass
+
 
 def run_sql(sql, param=None, n=0, with_desc=False, with_dict=False, run_on_slave=False, connection=None):
     """Run SQL on the server with PARAM and return result.
@@ -239,7 +229,7 @@ def run_sql(sql, param=None, n=0, with_desc=False, with_dict=False, run_on_slave
         # do not connect to the database as the site is closed for maintenance:
         return []
     elif CFG_ACCESS_CONTROL_LEVEL_SITE > 0:
-        ## Read only website
+        # Read only website
         if not sql.upper().startswith("SELECT") and not sql.upper().startswith("SHOW"):
             return
 
@@ -250,37 +240,38 @@ def run_sql(sql, param=None, n=0, with_desc=False, with_dict=False, run_on_slave
     if run_on_slave and CFG_DATABASE_SLAVE:
         dbhost = CFG_DATABASE_SLAVE
 
-    ### log_sql_query(dbhost, sql, param) ### UNCOMMENT ONLY IF you REALLY want to log all queries
+    # log_sql_query(dbhost, sql, param) ### UNCOMMENT ONLY IF you REALLY want
+    # to log all queries
     try:
         db = connection or _db_login(dbhost)
         cur = db.cursor()
         gc.disable()
         rc = cur.execute(sql, param)
         gc.enable()
-    except (OperationalError, InterfaceError): # unexpected disconnect, bad malloc error, etc
-        # FIXME: now reconnect is always forced, we may perhaps want to ping() first?
+    # unexpected disconnect, bad malloc error, etc
+    except (OperationalError, InterfaceError):
+        # FIXME: now reconnect is always forced, we may perhaps want to ping()
+        # first?
         if connection is not None:
             raise
-        try:
-            db = _db_login(dbhost, relogin=1)
-            cur = db.cursor()
-            gc.disable()
-            rc = cur.execute(sql, param)
-            gc.enable()
-        except (OperationalError, InterfaceError): # unexpected disconnect, bad malloc error, etc
-            raise
+        db = _db_login(dbhost, relogin=1)
+        cur = db.cursor()
+        gc.disable()
+        rc = cur.execute(sql, param)
+        gc.enable()
 
-    if string.upper(string.split(sql)[0]) in ("SELECT", "SHOW", "DESC", "DESCRIBE"):
+    if sql.split()[0].upper() in ("SELECT", "SHOW", "DESC", "DESCRIBE"):
         if n:
             recset = cur.fetchmany(n)
         else:
             recset = cur.fetchall()
 
-        if with_dict: # return list of dictionaries
+        if with_dict:  # return list of dictionaries
             # let's extract column names
             keys = [row[0] for row in cur.description]
             # let's construct a list of dictionaries
-            list_dict_results = [dict(zip(*[keys, values])) for values in recset]
+            list_dict_results = [dict(zip(keys, values))
+                                 for values in recset]
 
             if with_desc:
                 return list_dict_results, cur.description
@@ -292,9 +283,10 @@ def run_sql(sql, param=None, n=0, with_desc=False, with_dict=False, run_on_slave
             else:
                 return recset
     else:
-        if string.upper(string.split(sql)[0]) == "INSERT":
+        if sql.split()[0].upper() == "INSERT":
             rc = cur.lastrowid
         return rc
+
 
 def run_sql_many(query, params, limit=CFG_MISCUTIL_SQL_RUN_SQL_MANY_LIMIT, run_on_slave=False):
     """Run SQL on the server with PARAM.
@@ -314,7 +306,7 @@ def run_sql_many(query, params, limit=CFG_MISCUTIL_SQL_RUN_SQL_MANY_LIMIT, run_o
         # do not connect to the database as the site is closed for maintenance:
         return []
     elif CFG_ACCESS_CONTROL_LEVEL_SITE > 0:
-        ## Read only website
+        # Read only website
         if not query.upper().startswith("SELECT") and not query.upper().startswith("SHOW"):
             return
 
@@ -324,7 +316,7 @@ def run_sql_many(query, params, limit=CFG_MISCUTIL_SQL_RUN_SQL_MANY_LIMIT, run_o
     i = 0
     r = None
     while i < len(params):
-        ## make partial query safely (mimicking procedure from run_sql())
+        # make partial query safely (mimicking procedure from run_sql())
         try:
             db = _db_login(dbhost)
             cur = db.cursor()
@@ -332,21 +324,19 @@ def run_sql_many(query, params, limit=CFG_MISCUTIL_SQL_RUN_SQL_MANY_LIMIT, run_o
             rc = cur.executemany(query, params[i:i + limit])
             gc.enable()
         except (OperationalError, InterfaceError):
-            try:
-                db = _db_login(dbhost, relogin=1)
-                cur = db.cursor()
-                gc.disable()
-                rc = cur.executemany(query, params[i:i + limit])
-                gc.enable()
-            except (OperationalError, InterfaceError):
-                raise
-        ## collect its result:
+            db = _db_login(dbhost, relogin=1)
+            cur = db.cursor()
+            gc.disable()
+            rc = cur.executemany(query, params[i:i + limit])
+            gc.enable()
+        # collect its result:
         if r is None:
             r = rc
         else:
             r += rc
         i += limit
     return r
+
 
 def run_sql_with_limit(query, param=None, n=0, with_desc=False, wildcard_limit=0, run_on_slave=False):
     """This function should be used in some cases, instead of run_sql function, in order
@@ -355,17 +345,15 @@ def run_sql_with_limit(query, param=None, n=0, with_desc=False, wildcard_limit=0
         The parameters are exactly the ones for run_sql function.
         In case the query limit is reached, an InvenioDbQueryWildcardLimitError will be raised.
     """
-    try:
-        dummy = int(wildcard_limit)
-    except ValueError:
-        raise
-    if wildcard_limit < 1:#no limit on the wildcard queries
+    dummy = int(wildcard_limit)
+    if wildcard_limit < 1:  # no limit on the wildcard queries
         return run_sql(query, param, n, with_desc, run_on_slave=run_on_slave)
-    safe_query = query + " limit %s" %wildcard_limit
+    safe_query = query + " limit %s" % wildcard_limit
     res = run_sql(safe_query, param, n, with_desc, run_on_slave=run_on_slave)
     if len(res) == wildcard_limit:
         raise InvenioDbQueryWildcardLimitError(res)
     return res
+
 
 def blob_to_string(ablob):
     """Return string representation of ABLOB.  Useful to treat MySQL
@@ -381,6 +369,7 @@ def blob_to_string(ablob):
     else:
         return ablob
 
+
 def log_sql_query(dbhost, sql, param=None):
     """Log SQL query into prefix/var/log/dbquery.log log file.  In order
        to enable logging of all SQL queries, please uncomment one line
@@ -392,16 +381,18 @@ def log_sql_query(dbhost, sql, param=None):
     log_path = CFG_LOGDIR + '/dbquery.log'
     date_of_log = convert_datestruct_to_datetext(time.localtime())
     message = date_of_log + '-->\n'
-    message += indent_text('Host:\n' + indent_text(str(dbhost), 2, wrap=True), 2)
+    message += indent_text('Host:\n' +
+                           indent_text(str(dbhost), 2, wrap=True), 2)
     message += indent_text('Query:\n' + indent_text(str(sql), 2, wrap=True), 2)
-    message += indent_text('Params:\n' + indent_text(str(param), 2, wrap=True), 2)
+    message += indent_text('Params:\n' +
+                           indent_text(str(param), 2, wrap=True), 2)
     message += '-----------------------------\n\n'
     try:
-        log_file = open(log_path, 'a+')
-        log_file.writelines(message)
-        log_file.close()
-    except:
+        with open(log_path, 'a+') as log_file:
+            log_file.writelines(message)
+    except (IOError, OSError):
         pass
+
 
 def get_table_update_time(tablename, run_on_slave=False):
     """Return update time of TABLENAME.  TABLENAME can contain
@@ -419,10 +410,9 @@ def get_table_update_time(tablename, run_on_slave=False):
     # table_name='collection'.
     res = run_sql("SHOW TABLE STATUS LIKE %s", (tablename,),
                   run_on_slave=run_on_slave)
-    update_times = [] # store all update times
+    update_times = []  # store all update times
     for row in res:
-        if type(row[10]) is long or \
-           row[10] is None:
+        if type(row[10]) is long or row[10] is None:
             # MySQL-4.1 and 5.0 have creation_time in 11th position,
             # so return next column:
             update_times.append(str(row[12]))
@@ -433,6 +423,7 @@ def get_table_update_time(tablename, run_on_slave=False):
             update_times.append(str(row[11]))
     return max(update_times)
 
+
 def get_table_status_info(tablename, run_on_slave=False):
     """Return table status information on TABLENAME.  Returned is a
        dict with keys like Name, Rows, Data_length, Max_data_length,
@@ -441,10 +432,9 @@ def get_table_status_info(tablename, run_on_slave=False):
     # Note: again a hack so that it works on all MySQL 4.0, 4.1, 5.0
     res = run_sql("SHOW TABLE STATUS LIKE %s", (tablename,),
                   run_on_slave=run_on_slave)
-    table_status_info = {} # store all update times
+    table_status_info = {}  # store all update times
     for row in res:
-        if type(row[10]) is long or \
-           row[10] is None:
+        if type(row[10]) is long or row[10] is None:
             # MySQL-4.1 and 5.0 have creation time in 11th position:
             table_status_info['Name'] = row[0]
             table_status_info['Rows'] = row[4]
@@ -464,13 +454,16 @@ def get_table_status_info(tablename, run_on_slave=False):
             table_status_info['Update_time'] = row[11]
     return table_status_info
 
+
 def serialize_via_marshal(obj):
     """Serialize Python object via marshal into a compressed string."""
     return compress(marshal.dumps(obj))
 
+
 def deserialize_via_marshal(astring):
     """Decompress and deserialize string into a Python object via marshal."""
     return marshal.loads(decompress(astring))
+
 
 def wash_table_column_name(colname):
     """
@@ -485,9 +478,10 @@ def wash_table_column_name(colname):
 
     @raise Exception: Raises an exception if colname is invalid.
     """
-    if re.search('[^\w]', colname):
+    if re.search(r'[^\w]', colname):
         raise Exception('The table column %s is not valid.' % repr(colname))
     return colname
+
 
 def real_escape_string(unescaped_string, run_on_slave=False):
     """
